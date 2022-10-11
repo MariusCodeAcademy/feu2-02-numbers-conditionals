4f41c99eca76272434385ac43798568ddcd60862
Marius <marius@example.com>
2022-10-11 09:04:40 +0300

    uzd math

---
+++ js/math.js
@@ -0,0 +1,16 @@
+// atvaizduoja daugiau klaidu.
+'use strict';
+console.log('math.js');
+
+let num1 = 11.458721;
+
+// Math.roud()
+
+let rounded = Math.round(num1);
+console.log('rounded', rounded);
+
+// floor
+// ceil
+// toFixed
+// surasti kaip is 24 istraukti kvadratine sakni (Math)
+// pakelti 5, 7 laipsniu
